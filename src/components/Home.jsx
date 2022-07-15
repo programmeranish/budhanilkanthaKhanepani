8f40d60dc3c01d7100e4c39609e1836c92e2ee9b
Anish Shrestha <me.anish.shrestha@gmail.com>
2022-07-15 18:08:56 +0545

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  console.log("hello");
+  return (
+    <div>
+      <video id="waterfall" src="waterfall.mp4" muted loop autoPlay></video>
+      <h1>Budhanilkantha khanepani</h1>
+      <nav>this is navbar</nav>
+    </div>
+  );
+}
+
+export default Home;
