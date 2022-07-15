85d2314e5a595741e6a6739f7145325e2c538e75
programmeranish <programmer.anish.shrestha@gmail.com>
2022-07-15 21:22:32 +0545

    navbar start

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,18 @@ function Home() {
   console.log("hello");
   return (
     <div>
-      <video id="waterfall" src="waterfall.mp4" muted loop autoPlay></video>
-      <h1>Budhanilkantha khanepani</h1>
-      <nav>this is navbar</nav>
+      <video id="waterfall" src="waterfall.mp4" type="video/mp4" muted loop autoPlay></video>
+      <nav>
+        <ul className="navItems">
+          <li>Home</li>
+          <li>Pay Bills</li>
+          <li>Report</li>
+          <li className="navbar-brand">Budhanilkantha khanepani</li>
+          <li>Location</li>
+          <li>Services</li>
+          <li>Maps</li>
+        </ul>
+      </nav>
     </div>
   );
 }
