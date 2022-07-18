8c9b8c4da66614294878050935442327068c4fc5
anish shrestha <programmer.anish.shrestha@gmail.com>
2022-07-18 13:21:28 +0545

    navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,35 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 function Home() {
-  let [isMenuDisplay, setMenuDisplay] = useState(false);
+  let [isLinksDisplay, setLinksDisplay] = useState(true);
 
   useEffect(() => {
     window.addEventListener("resize", () => {
-      if (window.innerWidth > 1300) setMenuDisplay(true);
+      if (window.innerWidth > 1300) setLinksDisplay(true);
     });
   }, []);
 
   function handleMenuClick() {
-    console.log("before", isMenuDisplay);
-
-    setMenuDisplay((isMenuDisplay) => {
-      return !isMenuDisplay;
+    setLinksDisplay((isLinksDisplay) => {
+      return !isLinksDisplay;
     });
-    console.log(isMenuDisplay);
   }
   return (
     <div>
-      <video id="waterfall" src="waterfall.mp4" type="video/mp4" muted loop autoPlay></video>
+      {/* <video id="waterfall" src="waterfall.mp4" type="video/mp4" muted loop autoPlay></video> */}
       <nav>
         <div className="navbar-brand">Budhanilkantha khanepani</div>
-        {isMenuDisplay && (
+        {isLinksDisplay && (
           <ul className="navItems">
             <li>Home</li>
             <li>Pay Bills</li>
             <li>Report</li>
             <li>Location</li>
             <li>Services</li>
-            <li>Maps</li>
+            <li>Login</li>
           </ul>
         )}
         <img id="hamburgerMenu" src="hamburger-menu.svg" alt="hamburger menu" onClick={handleMenuClick} />
